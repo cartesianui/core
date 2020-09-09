ee371169c734a277d34feb98179f096394825fa5
Syed Ali Murtaza <ali@kazmi.me>
2020-09-09 21:26:08 +0500

    Updates validation service to inject in root module.

--- projects/ng-axis/src/lib/services/utils/validation.service.ts
+++ projects/ng-axis/src/lib/services/utils/validation.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { 
+import {
   FormControl,
   FormGroup
 }                     from '@angular/forms';
-@Injectable()
+@Injectable({
+  providedIn: "root",
+})
 export class ValidationService {
 
   /**
