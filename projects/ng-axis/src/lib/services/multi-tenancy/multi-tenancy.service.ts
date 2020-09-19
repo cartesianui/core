493206f989653f2d2e8e6139479238a6380a0a3c
Syed Ali Murtaza <ali@kazmi.me>
2020-09-19 14:55:02 +0500

    1. Renames AxisMultiTenancyService to MultiTenancyService. 2. Removes AxisSessionService, as effect of

--- projects/ng-axis/src/lib/services/multi-tenancy/multi-tenancy.service.ts
+++ projects/ng-axis/src/lib/services/multi-tenancy/multi-tenancy.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
-export class AxisMultiTenancyService {
+export class MultiTenancyService {
   get isEnabled(): boolean {
     return axis.multiTenancy.isEnabled;
   }
