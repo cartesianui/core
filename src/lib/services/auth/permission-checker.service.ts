e13a3909d1925f2a4ae5868017c1791525782602
Syed Ali Murtaza <ali@kazmi.me>
2022-01-28 11:55:05 +0500

    Updates permission checker service, to check any or all permissions

--- src/lib/services/auth/permission-checker.service.ts
+++ src/lib/services/auth/permission-checker.service.ts
@@ -9,4 +9,12 @@ export class PermissionCheckerService {
   isGranted(permissionName: string): boolean {
     return axis.auth.isGranted(permissionName);
   }
+
+  isAnyGranted(permissions: string[]): boolean {
+    return axis.auth.isAnyGranted(...permissions);
+  }
+
+  areAllGranted(permissions: string[]): boolean {
+    return axis.auth.areAllGranted(...permissions);
+  }
 }
