4079a97c56e68fe1090561deedc95ae57d04cb28
Syed Ali Murtaza <ali@kazmi.me>
2022-01-28 11:55:39 +0500

    Implements session service

---
+++ src/lib/services/session/session.service.ts
@@ -0,0 +1,36 @@
+﻿///<reference path="../../../../../../node_modules/@cartesian-ui/js-axis/axis.d.ts"/>
+
+import {Injectable, Injector} from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SessionService {
+
+  private _session = null;
+
+  constructor( private _injector: Injector ) {
+    this._session = axis.session;
+  }
+
+  get isHostSide(): boolean {
+    return this._session.isHostSide();
+  }
+
+  get isHostAdmin(): boolean {
+    return this._session.isHostAdmin();
+  }
+
+  get isTenantSide(): boolean {
+    return this._session.isTenantSide();
+  }
+
+  get isTenantAdmin(): boolean {
+    return this._session.isTenantAdmin();
+  }
+
+  get isUserLogged(): boolean {
+    return this._session.isUserLogged();
+  }
+
+}
