1ec8e056e6239f027308e60578db35094b8235b9
Syed Ali Murtaza <ali@kazmi.me>
2022-06-06 12:28:07 +0500

    Adds index file with lib, to manage top level exports in public-api.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of ng-axis
+ */
+
+export * from './app-constants';
+export * from './axis.module';
+export * from './services';
+export * from './interceptors';
+export * from './models';
+export * from './async/http';
