86379d47554b4943e198ccd6ae44323a569851f6
Syed Ali Murtaza <ali@kazmi.me>
2023-10-17 09:42:12 +0500

    Updates error notification service to convert response keyys from snake to camel case

--- src/lib/async/http/http-notification.service.ts
+++ src/lib/async/http/http-notification.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { AppConstants } from '../../app-constants';
 import { IError } from './types';
+import { convertObjectKeysToCamel } from '../../services/utils/helpers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpNotificationService {
-
   public serverErrors$ = new Subject<IError>();
 
+  static convertResponseObjectKeysToCamel(response: any) {
+    return Object.assign({}, convertObjectKeysToCamel(response));
+  }
+
   dispatch(errors: IError) {
+    if (AppConstants.convertResponseObjectKeysToCamel) {
+      errors = HttpNotificationService.convertResponseObjectKeysToCamel(errors);
+    }
+
     this.serverErrors$.next(errors);
   }
 }
